7c96043ae20cb1f111309b3959fe441837a131a7
mesaimemoria <50259637+mesaimemoria@users.noreply.github.com>
2019-10-31 08:06:48 +0800

    new update

--- login-signup-ui-new/lib/home_page.dart
+++ login-signup-ui-new/lib/home_page.dart
@@ -1,10 +1,189 @@
 import 'package:flutter/material.dart';
+import 'package:sliding_up_panel/sliding_up_panel.dart';
+import 'package:login/login_page.dart';
 
 class HomePage extends StatelessWidget {
   static String tag = 'home-page';
 
   @override
   Widget build(BuildContext context) {
+    final ratings_and_reviews = Padding(
+      padding: EdgeInsets.only(top: 20.0),
+      child: Text(
+        'RATINGS AND REVIEWS',
+        style: TextStyle(fontSize: 15.0, color: Colors.blueGrey),
+      ),
+    );
+
+    final ratings = Padding(
+      padding: EdgeInsets.only(top: 25.0, bottom: 20.0),
+      child: Text(
+        'RATINGS',
+        style: TextStyle(fontSize: 25.0, color: Colors.blueGrey),
+      ),
+    );
+
+    final restaurants = Row(
+      children: <Widget>[
+        Expanded(
+          flex: 8,
+          child: Text(
+            'Restaurants',
+            style: TextStyle(fontSize: 18.0, color: Colors.orangeAccent),
+          ),
+        ),
+        Expanded(
+          flex: 2,
+          child: Text(
+            '4.5',
+            style: TextStyle(fontSize: 18.0, color: Colors.blueGrey),
+          ),
+        ),
+      ],
+    );
+
+    final hotels_living_space = Row(
+      children: <Widget>[
+        Expanded(
+          flex: 8,
+          child: Text(
+            'Hotels/Living Space',
+            style: TextStyle(fontSize: 18.0, color: Colors.green),
+          ),
+        ),
+        Expanded(
+          flex: 2,
+          child: Text(
+            '5.0',
+            style: TextStyle(fontSize: 18.0, color: Colors.blueGrey),
+          ),
+        ),
+      ],
+    );
+
+    final hospitality = Row(
+      children: <Widget>[
+        Expanded(
+          flex: 8,
+          child: Text(
+            'Hospitality',
+            style: TextStyle(fontSize: 18.0, color: Colors.red),
+          ),
+        ),
+        Expanded(
+          flex: 2,
+          child: Text(
+            '4.0',
+            style: TextStyle(fontSize: 18.0, color: Colors.blueGrey),
+          ),
+        ),
+      ],
+    );
+
+    final transportation = Row(
+      children: <Widget>[
+        Expanded(
+          flex: 8,
+          child: Text(
+            'Transportation',
+            style: TextStyle(fontSize: 18.0, color: Colors.pinkAccent),
+          ),
+        ),
+        Expanded(
+          flex: 2,
+          child: Text(
+            '3.0',
+            style: TextStyle(fontSize: 18.0, color: Colors.blueGrey),
+          ),
+        ),
+      ],
+    );
+
+    final reviews = Padding(
+      padding: EdgeInsets.only(top: 30.0),
+      child: Text(
+        'REVIEWS',
+        style: TextStyle(fontSize: 25.0, color: Colors.blueGrey),
+      ),
+    );
+
+    final addButton = Padding(
+      padding: EdgeInsets.only(left: 200.0),
+      child: RaisedButton(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(24),
+        ),
+        padding: EdgeInsets.all(12),
+        color: Colors.blueGrey,
+        child: Text('ADD', style: TextStyle(color: Colors.white)),
+        onPressed: () {
+//          Navigator.push(
+//              context,
+//              MaterialPageRoute(builder: (context) => Add()));
+        },
+      ),
+    );
+
+
+
+    final person = Row(
+        children: [
+          Image(image: AssetImage('assets/alucard.jpg'), width: 50,),
+          Column(
+        children: [Padding(
+      padding: EdgeInsets.only(top: 10.0, right: 10.0, left: 20),
+      child: Text(
+        'Ardel Unknown',
+        style: TextStyle(fontSize: 20.0, color: Colors.blueGrey),
+      ),
+    ),
+
+    Padding(
+      padding: EdgeInsets.only(right: 10.0, left: 15),
+      child: Text(
+        'Brgy Villamonte, Spice Kitchen',
+        style: TextStyle(fontSize: 12.0, color: Colors.blueGrey),
+      ),
+    ),
+    ],
+    ),
+      Image(image: AssetImage('assets/stars.png'), width: 100,),
+    ],
+    );
+
+    final comment = Padding(
+      padding: EdgeInsets.only(right: 10.0, top: 10.0),
+      child: Text(
+        'I went there and I liked it very much. Awesome!I went there'
+            'and I liked it very much. Awesome!'
+            'I went there and I liked it very much. Awesome!'
+            'I went there and I liked it very much',
+        style: TextStyle(fontSize: 12.0, color: Colors.blueGrey),
+      ),
+    );
+
+    final tag = Padding(
+      padding: EdgeInsets.only(left: 230),
+        child: RaisedButton(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(24),
+        ),
+      padding: EdgeInsets.all(8),
+      color: Colors.orange,
+      child: Text('Restaurants', style: TextStyle(color: Colors.white)),
+        ),
+    );
+
+
+    final body = Container(
+      width: MediaQuery.of(context).size.width,
+      padding: EdgeInsets.all(28.0),
+      child: Column(
+        children: <Widget>[ratings_and_reviews, ratings, restaurants, hotels_living_space, hospitality, transportation,
+          reviews, addButton, person, comment, tag, SizedBox(height: 40), person, comment, SizedBox(height: 40), person, comment],
+      ),
+    );
+
     final alucard = Hero(
       tag: 'hero',
       child: Padding(
@@ -26,22 +205,244 @@ class HomePage extends StatelessWidget {
     );
 
 
-    final body = Container(
-      width: MediaQuery.of(context).size.width,
-      padding: EdgeInsets.all(28.0),
-      decoration: BoxDecoration(
-        gradient: LinearGradient(colors: [
-          Colors.purpleAccent,
-          Colors.blue,
-        ]),
-      ),
-      child: Column(
-        children: <Widget>[alucard, welcome],
-      ),
-    );
+//    final body = Container(
+//      width: MediaQuery.of(context).size.width,
+//      padding: EdgeInsets.all(28.0),
+//      decoration: BoxDecoration(
+//        gradient: LinearGradient(colors: [
+//          Colors.purpleAccent,
+//          Colors.blue,
+//        ]),
+//      ),
+//      child: Column(
+//        children: <Widget>[alucard, welcome],
+//      ),
+//    );
 
     return Scaffold(
-      body: body,
+
+      appBar: AppBar(
+        iconTheme: new IconThemeData(color: Colors.blueGrey),
+        backgroundColor: Colors.white,
+          actions: <Widget>[
+            Container(
+              margin: const EdgeInsets.only(right: 20.0),
+              width: 35.0,
+              child: IconButton(
+                icon: IconButton(
+                  icon: Icon(
+                    Icons.search,
+                    color: Colors.blueGrey,
+                  ),
+                ),
+                onPressed: (){},
+              ),
+            ),
+          ],
+      ),
+      drawer: Padding(
+        padding: const EdgeInsets.only(top: 80.0),
+        child: Container(
+          width: 320.0,
+          child: new Drawer(
+            child: new ListView(
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.only(top: 0.0, left: 20.0),
+                  child: Text("Welcome First Name",
+                    style: TextStyle(
+                      fontSize: 25.0,
+                      fontWeight: FontWeight.w600,
+                      fontFamily: 'Nunito',
+                    ),
+                  ),
+                ),
+               Padding(
+                  padding: const EdgeInsets.only(left: 15.0),
+                  child: new  FlatButton(
+                    child: ListTile(
+                    title: new Text("Profile",
+                      style: TextStyle(
+                        fontSize: 18.0,
+                      ),
+                    ),
+                  ),
+                    onPressed: () {
+//                    Navigator.push(
+//                        context,
+//                        MaterialPageRoute(builder: (context) => SignupPage()));
+                    },
+                    ),
+
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 15.0),
+                  child: new  FlatButton(
+                    child: ListTile(
+                      title: new Text("Subscription",
+                        style: TextStyle(
+                          fontSize: 18.0,
+                        ),
+                      ),
+                    ),
+                    onPressed: () {
+//                    Navigator.push(
+//                        context,
+//                        MaterialPageRoute(builder: (context) => SignupPage()));
+                    },
+                  ),
+
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 15.0),
+                  child: new  FlatButton(
+                    child: ListTile(
+                      title: new Text("FEATURED PLACES",
+                        style: TextStyle(
+                          fontSize: 18.0,
+                          color: Colors.orange,
+                        ),
+                      ),
+                    ),
+                    onPressed: () {
+//                    Navigator.push(
+//                        context,
+//                        MaterialPageRoute(builder: (context) => SignupPage()));
+                    },
+                  ),
+
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 15.0),
+                  child: new  FlatButton(
+                    child: ListTile(
+                      title: new Text("Log Out",
+                        style: TextStyle(
+                          fontSize: 18.0,
+                          color: Colors.black,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ),
+                    onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => LoginPage()));
+                    },
+                  ),
+
+                ),
+                ],
+                ),
+
+            ),
+          ),
+        ),
+
+
+      body: SlidingUpPanel(
+        maxHeight: 2000,
+        minHeight: 260,
+        parallaxEnabled: true,
+        parallaxOffset: .5,
+        backdropEnabled: true,
+        panel: Center(
+          child: body,
+        ),
+        collapsed: Container(
+          decoration: BoxDecoration(color: Colors.blueGrey),
+          child: Column(
+              children: [Row(
+              children: [
+              Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget> [
+              Padding(
+              padding: const EdgeInsets.all(18.0),
+              child: Text('Bacolod City',
+              style: TextStyle(color: Colors.white, fontFamily: 'Nunito', fontSize: 28, fontWeight: FontWeight.w600),
+              ),
+            ),
+              Padding(
+                padding: const EdgeInsets.only(left: 50),
+                child: Text('Safety Level:',
+                  style: TextStyle(color: Colors.white, fontFamily: 'Nunito', fontSize: 20),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 140),
+                child: Text('90%',
+                  style: TextStyle(color: Colors.yellowAccent, fontFamily: 'Nunito', fontSize: 40),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 50),
+                child: Text('Tourist Density:',
+                  style: TextStyle(color: Colors.white, fontFamily: 'Nunito', fontSize: 20),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 140),
+                child: Text('10,000',
+                  style: TextStyle(color: Colors.yellowAccent, fontFamily: 'Nunito', fontSize: 20),
+                ),
+              ),
+        ],
+          ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                Padding(
+                padding: EdgeInsets.only(left: 75, top: 15),
+                child: RaisedButton(
+                shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(24),
+                ),
+                onPressed: ()  {
+
+                },
+                  padding: EdgeInsets.all(12),
+                  color: Colors.white,
+                  child: Text('Heat Map', style: TextStyle(color: Colors.blueGrey)),
+                ),
+                ),
+                  Padding(
+                    padding: EdgeInsets.only(left: 75, top: 15),
+                    child: RaisedButton(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(24),
+                      ),
+                      onPressed: ()  {
+
+                      },
+                      padding: EdgeInsets.all(12),
+                      color: Colors.white,
+                      child: Text('Icons Map', style: TextStyle(color: Colors.blueGrey)),
+                    ),
+                  ),
+                ],
+                ),
+
+          ],
+          ),
+          Column(
+            children: [
+              Container(
+            width: 800,
+            height: 50,
+            color: Colors.white,
+                child: Padding(
+                  padding: const EdgeInsets.all(14.0),
+                  child: Text('REVIEWS AND RATINGS', textAlign: TextAlign.center, style: TextStyle(fontWeight: FontWeight.w600),),
+                ),
+          ),
+          ],
+          ),
+              ],
+        ),
+    ),
+        body: Center(child: Text('dwadwd')),
+        ),
     );
   }
 }
--- login-signup-ui/lib/home_page.dart
+++ login-signup-ui/lib/home_page.dart
@@ -59,6 +59,186 @@ class _HomePageState extends State<HomePage> {
 
     return Scaffold(
       body: body,
+      drawer: Padding(
+        padding: const EdgeInsets.only(top: 80.0),
+        child: Container(
+          width: 380.0,
+          child: new Drawer(
+            child: new ListView(
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.only(top: 0.0, left: 20.0),
+                  child: Text("Explore Categories",
+                    style: TextStyle(
+                      fontSize: 25.0,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 15.0),
+                  child: new ListTile(
+                    title: new Text("All Listing",
+                      style: TextStyle(
+                        fontSize: 18.0,
+                      ),
+                    ),
+                    trailing: new Icon(Icons.arrow_right),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 15.0),
+                  child: new ListTile(
+                    title: new Text("Contests",
+                      style: TextStyle(
+                        fontSize: 18.0,
+                      ),
+                    ),
+                    trailing: new Icon(Icons.arrow_right),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 15.0),
+                  child: new ListTile(
+                    title: new Text("Lucky Draw",
+                      style: TextStyle(
+                        fontSize: 18.0,
+                      ),
+                    ),
+                    trailing: new Icon(Icons.arrow_right),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 15.0),
+                  child: new ListTile(
+                    title: new Text("Promotions",
+                      style: TextStyle(
+                        fontSize: 18.0,
+                      ),
+                    ),
+                    trailing: new Icon(Icons.arrow_right),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 15.0),
+                  child: new ListTile(
+                    title: new Text("Sweepstakes",
+                      style: TextStyle(
+                        fontSize: 18.0,
+                      ),
+                    ),
+                    trailing: new Icon(Icons.arrow_right),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 15.0),
+                  child: new ListTile(
+                    title: new Text("Featured",
+                      style: TextStyle(
+                        fontSize: 18.0,
+                      ),
+                    ),
+                    trailing: new Icon(Icons.arrow_right),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 15.0),
+                  child: new ListTile(
+                    title: new Text("What's Hot",
+                      style: TextStyle(
+                        fontSize: 18.0,
+                      ),
+                    ),
+                    trailing: new Icon(Icons.arrow_right),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 15.0),
+                  child: new ListTile(
+                    title: new Text("Others",
+                      style: TextStyle(
+                        fontSize: 18.0,
+                      ),
+                    ),
+                    trailing: new Icon(Icons.arrow_right),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 0.0, right: 250.0),
+                  child: CircleAvatar(
+                    backgroundColor: Colors.transparent,
+                    radius: 30.0,
+                    child: Image.asset('assets/logo.png'),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: Text("Every city has a unique vibe with plenty interesting finds waiting to be discovered. Lucky Village - contests, deals and promotions is here to provide you with the lates fresh happening in and around town on a variety of things.",
+                    style: TextStyle(
+                      color: Colors.grey[400],
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 0.0, right: 15.0, left: 15.0, bottom: 15.0),
+                  child: Text("Get Lucky and win more!",
+                    style: TextStyle(
+                      color: Colors.grey[400],
+                    ),
+                  ),
+                ),
+                Container(
+                  width: 1000,
+                  height: 70,
+                  color: Colors.grey[200],
+                  child: Padding(
+                    padding: const EdgeInsets.all(12.0),
+                    child: Column(
+                      children: <Widget>[
+                        Row(
+                          children: <Widget>[
+                            Text("Copyright 2018",
+                              style: TextStyle(
+                                color: Colors.grey[400],
+                              ),
+                            ),
+                            Text("Lucky Village - Contests and Promotions",
+                              style: TextStyle(
+                                color: Colors.black54,
+                              ),
+                            ),
+                          ],
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(left: 100.0),
+                          child: Row(
+                            children: <Widget>[
+                              Center(
+                                child: Text("Powered by",
+                                  style: TextStyle(
+                                    color: Colors.grey[400],
+                                  ),
+                                ),
+                              ),
+                              Center(
+                                child: Text("Hessedevotion",
+                                  style: TextStyle(
+                                    color: Colors.black54,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
